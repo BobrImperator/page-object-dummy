61f3bd84d9279c50171e0b61e25ba93009c9b6b7
BobrImperator <lordboberro@gmail.com>
2017-12-28 16:28:09 +0100

    error repair

--- tests/integration/components/item-list/component-test.js
+++ tests/integration/components/item-list/component-test.js
@@ -1,21 +1,17 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import page from 'page-object-dummy/tests/pages/item';
-// import wait from 'ember-test-helpers/wait';
+import wait from 'ember-test-helpers/wait';
 moduleForComponent('items-list', 'Integration | Component | items-list', {
   integration: true,
-  // beforeEach() {
-  //   this.server = startMirage();
-  //   page.setContext(this);
-  //   window.wait = wait;
-  // },
-  // afterEach() {
-  //   this.server.shutdown();
-  // },
+  beforeEach() {
+    page.setContext(this);
+    window.wait = wait;
+  }
 });
 test('it fills input', function(assert) {
   assert.expect(1);
   this.render(hbs`{{item-list}}`);
   page.name('Hello');
-  assert.equal(page.name().value, 'Hello', 'it displays');
+  assert.equal(this.$('input').val()  , 'Hello', 'it displays');
 });
